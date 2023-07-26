827e9d857c91a72562fba5e5b37a74f38661304d
Komron <komronabdujabbor@gmail.com>
2023-07-26 18:19:51 +0500

    talabalar kitobni sotib olishi mumkin

---
+++ BackEndService/Models/BoughtBooks.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEndService.Models
+{
+    public class BoughtBooks
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public Guid StudentId { get; set; }
+        public ICollection<Student> students { get; set; }
+        public Guid BookId { get; set; }
+        public ICollection<Book> books { get; set; }
+        public int Quantity { get; set; }
+        public DateTime BoughtTime { get; set; }
+
+    }
+}
