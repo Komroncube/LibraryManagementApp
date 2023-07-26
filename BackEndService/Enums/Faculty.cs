6f5450df38327956feaae6468d548711d481e05e
Komron <komronabdujabbor@gmail.com>
2023-07-26 18:19:34 +0500

    fakultetlar yozilyapti

---
+++ BackEndService/Enums/Faculty.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEndService.Enums
+{
+    public enum Faculty
+    {
+        Buxgalteriya_hisobi_va_audit=0,
+        Bank_ishi_va_audit,
+        Byudjet_hisobi_va_gaznachilik,
+        Soliq_va_sugurta,
+        Iqtisodiyot,
+        Moliya
+    }
+}
