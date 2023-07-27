df3f4b3d62da357889b2ee73ae4a9c51864a48e3
Komron <komronabdujabbor@gmail.com>
2023-07-27 17:42:12 +0500

    student crud srvice

---
+++ BackEndService/Service/StudentService.cs
@@ -0,0 +1,35 @@
+﻿namespace BackEndService.Service
+{
+    public class StudentService
+    {
+        private LibraryManagementDb _libdb = new LibraryManagementDb();
+        public void Create(Student student)
+        {
+            _libdb.Students.Add(student);
+            _libdb.SaveChanges();
+        }
+        public void Delete(Student student)
+        {
+            _libdb.Students.Remove(student);
+            _libdb.SaveChanges();
+
+        }
+        public IList<Student> GetAll()
+        {
+            return _libdb.Students.AsNoTracking().ToList();
+        }
+        public void Update(Student student)
+        {
+            var st=_libdb.Students.FirstOrDefault(x=>x.Id == student.Id);
+            if (st != null)
+            {
+                st.FirstName = student.FirstName;
+                st.LastName = student.LastName;
+                st.PhoneNumber = student.PhoneNumber;
+                st.Faculty = student.Faculty;
+                _libdb.SaveChanges();
+
+            }
+        }
+    }
+}
