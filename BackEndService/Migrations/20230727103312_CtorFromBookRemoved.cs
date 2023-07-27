b7bf1d4e10127827b79e330cfc48a741161d5439
Komron <komronabdujabbor@gmail.com>
2023-07-27 15:34:06 +0500

    some migrations, book edited

---
+++ BackEndService/Migrations/20230727103312_CtorFromBookRemoved.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackEndService.Migrations
+{
+    /// <inheritdoc />
+    public partial class CtorFromBookRemoved : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
