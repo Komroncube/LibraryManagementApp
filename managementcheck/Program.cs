f86f984426652a155bca86a92074cb9e2b4617a8
Komron <komronabdujabbor@gmail.com>
2023-07-27 17:43:16 +0500

    I don't know what is it?

--- managementcheck/Program.cs
+++ managementcheck/Program.cs
@@ -1,36 +1,49 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using BackEndService.Data;
 using BackEndService.Models;
 using BackEndService.Service;
 
-Console.WriteLine("Hello, World!");
-var book = new Book()
-{
-    Title = "My little prince",
-    Description = "bolalar va kattalarning dunyoqarashi farqi haqida",
-    Author = "Antuan de Sent Ekzyuperi",
-    Quantity = 100,
-    Price = 43,
+//Console.WriteLine("Hello, World!");
+//var book = new Book()
+//{
+//    Title = "My little prince",
+//    Description = "bolalar va kattalarning dunyoqarashi farqi haqida",
+//    Author = "Antuan de Sent Ekzyuperi",
+//    Quantity = 100,
+//    Price = 43,
 
-};
-BookService bookService = new BookService();
-//bookService.Create(book);
+//};
+//BookService bookService = new BookService();
+////bookService.Create(book);
 
-var st = new Student()
+//var st = new Student()
+//{
+//    FirstName = "Letti",
+//    LastName = "Ortiz",
+//    PhoneNumber = "12345678902",
+//    Faculty = BackEndService.Enums.Faculty.Buxgalteriya_hisobi_va_audit
+//};
+//StudentService stservice = new StudentService();
+////stservice.Create(st);
+//var lb = new Librarian()
+//{
+//    FirstName = "Teo",
+//    LastName = "Hernandez",
+//    PhoneNumber = "998374892304",
+//    UserName = "Teojon",
+//    Password = "Lz123",
+//};
+//LibrarianService ls = new LibrarianService();
+//ls.Create(lb);
+
+var db = new LibraryManagementDb();
+/*var kitoboldi = new BookStudent()
 {
-    FirstName = "Letti",
-    LastName = "Ortiz",
-    PhoneNumber = "12345678902",
-    Faculty = BackEndService.Enums.Faculty.Buxgalteriya_hisobi_va_audit
-};
-StudentService stservice = new StudentService();
-//stservice.Create(st);
-var lb = new Librarian()
-{
-    FirstName = "Teo",
-    LastName = "Hernandez",
-    PhoneNumber = "998374892304",
-    UserName = "Teojon",
-    Password = "Lz123",
-};
-LibrarianService ls = new LibrarianService();
-ls.Create(lb);
+    StudentId = db.Students.First().Id,
+    BookId = db.Books.First().Id,
+    Status = BackEndService.Enums.Status.Issued,
+    IssueDate = DateTime.UtcNow,
+};*/
+var qaytar=db.BookStudents.First();
+var bookoperations = new BookOperation();
+//bookoperations.ReturnBook(qaytar.Id);
