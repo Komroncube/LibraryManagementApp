20efc83951b0293cca079d6fc2f9a52cb75f15d2
Komron <komronabdujabbor@gmail.com>
2023-07-28 18:19:32 +0500

    enum default value changed

--- BackEndService/Enums/Faculty.cs
+++ BackEndService/Enums/Faculty.cs
@@ -8,7 +8,7 @@ namespace BackEndService.Enums
 {
     public enum Faculty
     {
-        Buxgalteriya_hisobi_va_audit=0,
+        Buxgalteriya_hisobi_va_audit=1,
         Bank_ishi_va_audit,
         Byudjet_hisobi_va_gaznachilik,
         Soliq_va_sugurta,
