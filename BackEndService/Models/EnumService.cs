6216e667cc37335e750a0f4cca19a8fb95a3fd53
Komron <komronabdujabbor@gmail.com>
2023-07-28 21:49:22 +0500

    enum to string string to enum

---
+++ BackEndService/Models/EnumService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEndService.Models
+{
+    public static class EnumService
+    {
+        public static string EnumToString(Faculty value)
+        {
+            return value.ToString().Replace("_", " ");
+        }
+        public static Faculty StringToEnum(string value)
+        {
+            return (Faculty)Enum.Parse(typeof(Faculty), value.Replace(" ", "_"));
+        }
+
+    }
+}
