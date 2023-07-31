566cefb1b1c0d057b40619359d0a26ee910892c8
Komron <komronabdujabbor@gmail.com>
2023-07-31 19:09:51 +0500

    Merged

---
+++ FrontEnd/IssueBookForm.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace FrontEnd
+{
+    partial class IssueBookForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // IssueBookForm
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1378, 820);
+            Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            Margin = new Padding(4, 4, 4, 4);
+            Name = "IssueBookForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "IssueBookForm";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
