da9a3d582f1bc3d4412a69231516d99cf3f58ab8
Komron <komronabdujabbor@gmail.com>
2023-08-01 21:00:57 +0500

    view model optimized for personal use

--- BackEndService/ViewModels/BookStudentView.cs
+++ BackEndService/ViewModels/BookStudentView.cs
@@ -6,10 +6,16 @@ using System.Threading.Tasks;
 
 namespace BackEndService.ViewModels
 {
-    public class StudentView
+    public class BookStudentView
     {
         public Guid Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public string Author { get; set; }
+        public Status Status { get; set; }
+        public int Quantity { get; set; }
     }
 }
