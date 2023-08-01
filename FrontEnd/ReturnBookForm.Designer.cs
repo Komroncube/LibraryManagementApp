649e50d4fd2f2ffe7d28c55b0fcd30fd393f87f4
Komron <komronabdujabbor@gmail.com>
2023-08-01 21:02:30 +0500

    returning book to library form

---
+++ FrontEnd/ReturnBookForm.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace FrontEnd
+{
+    partial class ReturnBookForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            returneddataview = new DataGridView();
+            issueddataview = new DataGridView();
+            return_btn = new Button();
+            book_input = new TextBox();
+            student_input = new TextBox();
+            book_lbl = new Label();
+            return_lbl = new Label();
+            issued_lbl = new Label();
+            student_lbl = new Label();
+            ((System.ComponentModel.ISupportInitialize)returneddataview).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)issueddataview).BeginInit();
+            SuspendLayout();
+            // 
+            // returneddataview
+            // 
+            returneddataview.AllowUserToAddRows = false;
+            returneddataview.AllowUserToDeleteRows = false;
+            returneddataview.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            returneddataview.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            returneddataview.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            returneddataview.Location = new Point(574, 425);
+            returneddataview.Name = "returneddataview";
+            returneddataview.ReadOnly = true;
+            returneddataview.RowHeadersWidth = 62;
+            returneddataview.RowTemplate.Height = 33;
+            returneddataview.Size = new Size(792, 309);
+            returneddataview.TabIndex = 11;
+            // 
+            // issueddataview
+            // 
+            issueddataview.AllowUserToAddRows = false;
+            issueddataview.AllowUserToDeleteRows = false;
+            issueddataview.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            issueddataview.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            issueddataview.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            issueddataview.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            issueddataview.Location = new Point(574, 66);
+            issueddataview.Name = "issueddataview";
+            issueddataview.ReadOnly = true;
+            issueddataview.RowHeadersWidth = 62;
+            issueddataview.RowTemplate.Height = 33;
+            issueddataview.Size = new Size(792, 257);
+            issueddataview.TabIndex = 12;
+            issueddataview.CellFormatting += issuedataGridView_CellFormatting;
+            // 
+            // return_btn
+            // 
+            return_btn.Location = new Point(152, 343);
+            return_btn.Name = "return_btn";
+            return_btn.Size = new Size(165, 78);
+            return_btn.TabIndex = 10;
+            return_btn.Text = "Return book";
+            return_btn.UseVisualStyleBackColor = true;
+            return_btn.Click += return_btn_Click;
+            // 
+            // book_input
+            // 
+            book_input.Font = new Font("Microsoft Sans Serif", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            book_input.Location = new Point(131, 242);
+            book_input.Name = "book_input";
+            book_input.PlaceholderText = "any information about book";
+            book_input.Size = new Size(260, 32);
+            book_input.TabIndex = 8;
+            book_input.TextChanged += book_input_TextChanged;
+            // 
+            // student_input
+            // 
+            student_input.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            student_input.Location = new Point(131, 147);
+            student_input.Name = "student_input";
+            student_input.PlaceholderText = "firstname or lastname";
+            student_input.Size = new Size(260, 35);
+            student_input.TabIndex = 9;
+            student_input.TextChanged += student_input_TextChanged;
+            // 
+            // book_lbl
+            // 
+            book_lbl.AutoSize = true;
+            book_lbl.Font = new Font("Microsoft Sans Serif", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            book_lbl.Location = new Point(12, 241);
+            book_lbl.Name = "book_lbl";
+            book_lbl.Size = new Size(79, 32);
+            book_lbl.TabIndex = 4;
+            book_lbl.Text = "Book";
+            // 
+            // return_lbl
+            // 
+            return_lbl.AutoSize = true;
+            return_lbl.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            return_lbl.Location = new Point(901, 360);
+            return_lbl.Name = "return_lbl";
+            return_lbl.Size = new Size(212, 38);
+            return_lbl.TabIndex = 5;
+            return_lbl.Text = "Returned books";
+            // 
+            // issued_lbl
+            // 
+            issued_lbl.AutoSize = true;
+            issued_lbl.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            issued_lbl.Location = new Point(919, 11);
+            issued_lbl.Name = "issued_lbl";
+            issued_lbl.Size = new Size(177, 38);
+            issued_lbl.TabIndex = 6;
+            issued_lbl.Text = "Issued books";
+            // 
+            // student_lbl
+            // 
+            student_lbl.AutoSize = true;
+            student_lbl.Font = new Font("Microsoft Sans Serif", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            student_lbl.Location = new Point(12, 145);
+            student_lbl.Name = "student_lbl";
+            student_lbl.Size = new Size(113, 32);
+            student_lbl.TabIndex = 7;
+            student_lbl.Text = "Student";
+            // 
+            // ReturnBookForm
+            // 
+            AutoScaleMode = AutoScaleMode.None;
+            ClientSize = new Size(1378, 744);
+            Controls.Add(returneddataview);
+            Controls.Add(issueddataview);
+            Controls.Add(return_btn);
+            Controls.Add(book_input);
+            Controls.Add(student_input);
+            Controls.Add(book_lbl);
+            Controls.Add(return_lbl);
+            Controls.Add(issued_lbl);
+            Controls.Add(student_lbl);
+            Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "ReturnBookForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "ReturnBookForm";
+            ((System.ComponentModel.ISupportInitialize)returneddataview).EndInit();
+            ((System.ComponentModel.ISupportInitialize)issueddataview).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView returneddataview;
+        private DataGridView issueddataview;
+        private Button return_btn;
+        private TextBox book_input;
+        private TextBox student_input;
+        private Label book_lbl;
+        private Label return_lbl;
+        private Label issued_lbl;
+        private Label student_lbl;
+    }
+}
---
+++ FrontEnd/ReturnBookForm.cs
@@ -0,0 +1,181 @@
+﻿using BackEndService.Service;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrontEnd
+{
+    public partial class ReturnBookForm : Form
+    {
+        DataTable issuedgridview;
+        DataTable returnedbookgridview;
+        BookOperation bookOperation;
+
+        public ReturnBookForm()
+        {
+            InitializeComponent();
+            student_lbl.Select();
+            bookOperation = new BookOperation();
+            
+            AddDataToTable(out issuedgridview,out returnedbookgridview);
+            issueddataview.DataSource = issuedgridview;
+            issueddataview.Columns[0].Visible = false;
+            issueddataview.Columns["Title"].DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+            issueddataview.Columns["Description"].DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+            issueddataview.Columns["Author"].DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+            
+            returneddataview.DataSource = returnedbookgridview;
+            returneddataview.Columns[0].Visible = false;
+            returneddataview.Columns["Title"].DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+            returneddataview.Columns["Description"].DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+            returneddataview.Columns["Author"].DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+
+
+
+        }
+        private void AddDataToTable(out DataTable issue, out DataTable returned)
+        {
+            var bookstudent = BookOperation.GetStudentBooks();
+            issue = new DataTable();
+            issue.Columns.Add("Id");
+            issue.Columns.Add("Student firstname");
+            issue.Columns.Add("Student lastname");
+            issue.Columns.Add("Phone number");
+            issue.Columns.Add("Title");
+            issue.Columns.Add("Description");
+            issue.Columns.Add("Author");
+
+
+            returned = new DataTable();
+            returned.Columns.Add("Id");
+            returned.Columns.Add("Student firstname");
+            returned.Columns.Add("Student lastname");
+            returned.Columns.Add("Phone number");
+            returned.Columns.Add("Title");
+            returned.Columns.Add("Description");
+            returned.Columns.Add("Author");
+            foreach (var book in bookstudent)
+            {
+                
+                if (book.Status == BackEndService.Enums.Status.Issued)
+                {
+                    issue.Rows.Add(book.Id, book.FirstName, book.LastName, book.PhoneNumber, book.Title, book.Description, book.Author);
+                }
+                else
+                {
+                    returned.Rows.Add(book.Id, book.FirstName, book.LastName, book.PhoneNumber, book.Title, book.Description, book.Author);
+
+                }
+            }
+
+        }
+        private void return_btn_Click(object sender, EventArgs e)
+        {
+            Guid id = Guid.Parse(issuedgridview.Rows[issueddataview.CurrentCell.RowIndex]["Id"].ToString());
+            bookOperation.ReturnBook(id);
+            student_input.Text = "";
+            book_input.Text = "";
+            MessageBox.Show("Book returned successfully");
+            AddDataToTable(out issuedgridview, out returnedbookgridview);
+            issueddataview.DataSource = issuedgridview;
+            returneddataview.DataSource = returnedbookgridview;
+        }
+
+        private void student_input_TextChanged(object sender, EventArgs e)
+        {
+            string searchText = student_input.Text.ToLower();
+
+            // Create a DataView of the DataTable
+            DataView dataView = new DataView(issuedgridview);
+
+            // Filter the rows of the DataView based on search text
+            dataView.RowFilter = string.Format("[{0}] LIKE '%{1}%' OR [{2}] LIKE '%{1}%' OR [{3}] LIKE '%{1}%'", "Student firstname", searchText, "Student lastname", "Phone number");
+            // Set the DataView as the data source of the DataGridView
+            issueddataview.DataSource = dataView;
+        }
+
+        private void book_input_TextChanged(object sender, EventArgs e)
+        {
+            string searchText = book_input.Text.ToLower();
+            // Create a DataView of the DataTable
+
+
+            DataView dataView = new DataView(issuedgridview);
+
+            // Filter the rows of the DataView based on search text
+            dataView.RowFilter = string.Format("[{0}] LIKE '%{1}%' OR [{2}] LIKE '%{1}%' OR [{3}] LIKE '%{1}%'", "Title", searchText, "Description", "Author");
+            // Set the DataView as the data source of the DataGridView
+            issueddataview.DataSource = dataView;
+        }
+
+
+        #region issue_editing_cell_size
+        private void issuedataGridView_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+            {
+                var cell = issueddataview.Rows[e.RowIndex].Cells[e.ColumnIndex];
+                if (cell != null && cell.Value != null)
+                {
+                    // Calculate the required height for the cell
+                    int preferredHeight = IssueCalculatePreferredRowHeight(issueddataview.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString());
+
+                    // Set the row height
+                    if (issueddataview.Rows[e.RowIndex].Height < preferredHeight)
+                    {
+                        issueddataview.Rows[e.RowIndex].Height = preferredHeight;
+                    }
+                }
+
+
+            }
+        }
+
+        private int IssueCalculatePreferredRowHeight(string cellText)
+        {
+            using (Graphics graphics = issueddataview.CreateGraphics())
+            {
+                SizeF size = graphics.MeasureString(cellText, issueddataview.Font, issueddataview.Columns["Description"].Width);
+                return (int)size.Height + 5; // Add some extra padding
+            }
+        }
+        #endregion editing_cell_size
+        #region return_editing_cell_size
+        private void return_dataGridView_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+            {
+                var cell = issueddataview.Rows[e.RowIndex].Cells[e.ColumnIndex];
+                if (cell != null && cell.Value != null)
+                {
+                    // Calculate the required height for the cell
+                    int preferredHeight = ReturnCalculatePreferredRowHeight(issueddataview.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString());
+
+                    // Set the row height
+                    if (issueddataview.Rows[e.RowIndex].Height < preferredHeight)
+                    {
+                        issueddataview.Rows[e.RowIndex].Height = preferredHeight;
+                    }
+                }
+
+
+            }
+        }
+
+        private int ReturnCalculatePreferredRowHeight(string cellText)
+        {
+            using (Graphics graphics = issueddataview.CreateGraphics())
+            {
+                SizeF size = graphics.MeasureString(cellText, issueddataview.Font, issueddataview.Columns["Description"].Width);
+                return (int)size.Height + 5; // Add some extra padding
+            }
+        }
+        #endregion editing_cell_size
+    }
+}
