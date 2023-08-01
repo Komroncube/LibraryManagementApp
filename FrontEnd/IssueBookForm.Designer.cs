9dbbe27e8eadb8282b82d1d46d126218e8b70ce2
Komron <komronabdujabbor@gmail.com>
2023-08-01 11:17:20 +0500

    issue book form added

---
+++ FrontEnd/IssueBookForm.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace FrontEnd
+{
+    partial class IssueBookForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            student_lbl = new Label();
+            book_lbl = new Label();
+            student_input = new TextBox();
+            book_input = new TextBox();
+            issue_btn = new Button();
+            studentdataview = new DataGridView();
+            bookdataview = new DataGridView();
+            label1 = new Label();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)studentdataview).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)bookdataview).BeginInit();
+            SuspendLayout();
+            // 
+            // student_lbl
+            // 
+            student_lbl.AutoSize = true;
+            student_lbl.Font = new Font("Microsoft Sans Serif", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            student_lbl.Location = new Point(12, 143);
+            student_lbl.Name = "student_lbl";
+            student_lbl.Size = new Size(113, 32);
+            student_lbl.TabIndex = 0;
+            student_lbl.Text = "Student";
+            // 
+            // book_lbl
+            // 
+            book_lbl.AutoSize = true;
+            book_lbl.Font = new Font("Microsoft Sans Serif", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            book_lbl.Location = new Point(12, 239);
+            book_lbl.Name = "book_lbl";
+            book_lbl.Size = new Size(79, 32);
+            book_lbl.TabIndex = 0;
+            book_lbl.Text = "Book";
+            // 
+            // student_input
+            // 
+            student_input.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            student_input.Location = new Point(131, 145);
+            student_input.Name = "student_input";
+            student_input.PlaceholderText = "firstname or lastname";
+            student_input.Size = new Size(260, 35);
+            student_input.TabIndex = 1;
+            student_input.TextChanged += student_input_TextChanged;
+            // 
+            // book_input
+            // 
+            book_input.Font = new Font("Microsoft Sans Serif", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            book_input.Location = new Point(131, 240);
+            book_input.Name = "book_input";
+            book_input.PlaceholderText = "any information about book";
+            book_input.Size = new Size(260, 32);
+            book_input.TabIndex = 1;
+            book_input.TextChanged += book_input_TextChanged;
+            // 
+            // issue_btn
+            // 
+            issue_btn.Location = new Point(152, 341);
+            issue_btn.Name = "issue_btn";
+            issue_btn.Size = new Size(151, 55);
+            issue_btn.TabIndex = 2;
+            issue_btn.Text = "Issue book";
+            issue_btn.UseVisualStyleBackColor = true;
+            issue_btn.Click += issue_btn_Click;
+            // 
+            // studentdataview
+            // 
+            studentdataview.AllowUserToAddRows = false;
+            studentdataview.AllowUserToDeleteRows = false;
+            studentdataview.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            studentdataview.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            studentdataview.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            studentdataview.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            studentdataview.Location = new Point(574, 64);
+            studentdataview.Name = "studentdataview";
+            studentdataview.ReadOnly = true;
+            studentdataview.RowHeadersWidth = 62;
+            studentdataview.RowTemplate.Height = 33;
+            studentdataview.Size = new Size(792, 257);
+            studentdataview.TabIndex = 3;
+            // 
+            // bookdataview
+            // 
+            bookdataview.AllowUserToAddRows = false;
+            bookdataview.AllowUserToDeleteRows = false;
+            bookdataview.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            bookdataview.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            bookdataview.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            bookdataview.Location = new Point(574, 423);
+            bookdataview.Name = "bookdataview";
+            bookdataview.ReadOnly = true;
+            bookdataview.RowHeadersWidth = 62;
+            bookdataview.RowTemplate.Height = 33;
+            bookdataview.Size = new Size(792, 309);
+            bookdataview.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(919, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(124, 38);
+            label1.TabIndex = 0;
+            label1.Text = "Students";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(936, 358);
+            label2.Name = "label2";
+            label2.Size = new Size(91, 38);
+            label2.TabIndex = 0;
+            label2.Text = "Books";
+            // 
+            // IssueBookForm
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1378, 744);
+            Controls.Add(bookdataview);
+            Controls.Add(studentdataview);
+            Controls.Add(issue_btn);
+            Controls.Add(book_input);
+            Controls.Add(student_input);
+            Controls.Add(book_lbl);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(student_lbl);
+            Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(4);
+            Name = "IssueBookForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Issue book";
+            ((System.ComponentModel.ISupportInitialize)studentdataview).EndInit();
+            ((System.ComponentModel.ISupportInitialize)bookdataview).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label student_lbl;
+        private Label book_lbl;
+        private TextBox student_input;
+        private TextBox book_input;
+        private Button issue_btn;
+        private DataGridView studentdataview;
+        private DataGridView bookdataview;
+        private Label label1;
+        private Label label2;
+    }
+}
---
+++ FrontEnd/IssueBookForm.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrontEnd
+{
+    public partial class IssueBookForm : Form
+    {
+        DataTable studentgridview;
+        DataTable bookgridview;
+        BookOperation bookOperation;
+        public IssueBookForm()
+        {
+            InitializeComponent();
+            student_lbl.Select();
+            bookOperation = new BookOperation();
+            //student table
+            List<Student> students = new StudentService().GetAll();
+            studentgridview = new DataTable();
+            studentgridview.Columns.Add("Id");
+            studentgridview.Columns.Add("First name");
+            studentgridview.Columns.Add("Last name");
+            studentgridview.Columns.Add("Phone number");
+            foreach (Student student in students)
+            {
+                studentgridview.Rows.Add(student.Id, student.FirstName, student.LastName, student.PhoneNumber);
+            }
+            studentdataview.DataSource = studentgridview;
+            studentdataview.Columns[0].Visible = false;
+
+            //book table
+            List<Book> books = new BookService().GetAll();
+            bookgridview = new DataTable();
+            bookgridview.Columns.Add("Id");
+            bookgridview.Columns.Add("Title");
+            bookgridview.Columns.Add("Description");
+            bookgridview.Columns.Add("Author");
+            bookgridview.Columns.Add("Quantity");
+            foreach (Book book in books)
+            {
+                if(book.Quantity> 0)
+                    bookgridview.Rows.Add(book.Id, book.Title, book.Description, book.Author, book.Quantity);
+            }
+            bookdataview.DataSource = bookgridview;
+            bookdataview.Columns[0].Visible = false;
+        }
+        private void student_input_TextChanged(object sender, EventArgs e)
+        {
+            string searchText = student_input.Text.ToLower();
+
+            // Create a DataView of the DataTable
+            DataView dataView = new DataView(studentgridview);
+
+            // Filter the rows of the DataView based on search text
+            dataView.RowFilter = string.Format("[{0}] LIKE '%{1}%' OR [{2}] LIKE '%{1}%' OR [{3}] LIKE '%{1}%'", "First name", searchText, "Last name", "Phone number");
+            // Set the DataView as the data source of the DataGridView
+            studentdataview.DataSource = dataView;
+        }
+
+        private void book_input_TextChanged(object sender, EventArgs e)
+        {
+            string searchText = book_input.Text.ToLower();
+            // Create a DataView of the DataTable
+            
+            
+            DataView dataView = new DataView(bookgridview);
+
+            // Filter the rows of the DataView based on search text
+            dataView.RowFilter = string.Format("[{0}] LIKE '%{1}%' OR [{2}] LIKE '%{1}%' OR [{3}] LIKE '%{1}%'", "Title", searchText, "Description", "Author");
+            // Set the DataView as the data source of the DataGridView
+            bookdataview.DataSource = dataView;
+        }   
+
+        private void issue_btn_Click(object sender, EventArgs e)
+        {
+            var issuebook = new BookStudent()
+            {
+                StudentId = Guid.Parse(studentgridview.Rows[studentdataview.CurrentCell.RowIndex]["Id"].ToString()),
+                BookId = Guid.Parse(bookgridview.Rows[bookdataview.CurrentCell.RowIndex]["Id"].ToString()),
+                Status = BackEndService.Enums.Status.Issued,
+                IssueDate = DateTime.UtcNow,
+            };
+            bookOperation.IssueBook(issuebook);
+            student_input.Text = "";
+            book_input.Text = "";
+            MessageBox.Show("Book issued successfully");
+            bookgridview.Rows[bookdataview.CurrentCell.RowIndex]["Id"].ToString();
+            int count = int.Parse(bookgridview.Rows[bookdataview.CurrentCell.RowIndex]["Quantity"].ToString());
+            count -= 1;
+            bookgridview.Rows[bookdataview.CurrentCell.RowIndex]["Quantity"]=count;
+            foreach (DataRow row in bookgridview.Rows)
+            {
+                if (int.Parse(row["Quantity"].ToString()) == 0)
+                {
+                    bookgridview.Rows.Remove(row);
+                }
+            }
+        }
+    }
+}
